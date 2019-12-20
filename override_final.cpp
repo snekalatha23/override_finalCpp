c39a1e38035419b16e17a9a8189bb68573184d4e
snekalatha23 <57349438+snekalatha23@users.noreply.github.com>
2019-12-20 11:49:11 +0530

    Create override_final.cpp

---
+++ override_final.cpp
@@ -0,0 +1,24 @@
+#include<iostream.h>
+#include<conio.h>
+
+
+class Base
+{
+    virtual void f(int);
+};
+
+class Derived : public Base
+{
+    virtual void f(float) override;  // Error
+};
+
+class A final
+{
+    virtual void f(int);
+};
+
+class B
+{
+    // no class can override f()
+    virtual void f() final; 
+};
